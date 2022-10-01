1754bbf83195e7ef07f614a5d459907d5568f545
Serge Khodorkovsky <serge.khodorkovsky@gmail.com>
2022-10-01 13:09:30 -0400

    2.1.1 release - Ender's Game

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1690,11 +1690,11 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -12.50
+#define Y_MIN_POS -6.50
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + X_MIN_POS
+#define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
